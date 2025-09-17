f7cfa92ec11ca3929a2041338a68a115e571aa1f
Brandon Benton <brandon.benton.5255@gmail.com>
2025-09-17 17:00:46 -0500

    JS for Hello World class JSX

--- Chapter_3/hello-world-class-jsx/js/script.js
+++ Chapter_3/hello-world-class-jsx/js/script.js
@@ -1,5 +1,16 @@
-ReactDOM.render(React.createElement(
+let helloWorldReactElement = React.createElement(
     'h1',
     null,
     'Hello world!'
-), document.getElementById('content'));
+);
+class HelloWorld extends React.Component {
+    render() {
+        return React.createElement(
+            'div',
+            null,
+            helloWorldReactElement,
+            helloWorldReactElement
+        );
+    }
+}
+ReactDOM.render(React.createElement(HelloWorld, null), document.getElementById('content'));
