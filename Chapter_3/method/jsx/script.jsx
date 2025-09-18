58719da3c316fdf4d4a8513bf562c28226477d72
Brandon Benton <brandon.benton.5255@gmail.com>
2025-09-18 11:11:50 -0500

    Define render function for Method Content component

--- Chapter_3/method/jsx/script.jsx
+++ Chapter_3/method/jsx/script.jsx
@@ -2,6 +2,13 @@ class Content extends React.Component {
     getUrl() {
         return 'http://webapplog.com'
     }
+    render() {
+        return (
+            <div>
+                <p>Your REST API URL is <a href={this.getUrl()}>{this.getUrl()}</a></p>
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(
