95f42ef5d3aff19bf8da0d107892bdd356eb64b5
Brandon Benton <brandon.benton.5255@gmail.com>
2025-09-18 10:56:20 -0500

    Define HelloWorld instances of HelloWorld class component

--- Chapter_3/hello-js-world-class-jsx/jsx/script.jsx
+++ Chapter_3/hello-js-world-class-jsx/jsx/script.jsx
@@ -5,5 +5,19 @@ class HelloWorld extends React.Component {
 }
 
 ReactDOM.render(
-    
+    <div>
+        <HelloWorld
+            id='ember'
+            frameworkName='Ember.js'
+            title='A framework for creating ambitious web applications.'/>
+        <HelloWorld
+            id='backbone'
+            frameworkName='Backbone.js'
+            title='Backbone.js gives structure to web applications...'/>
+        <HelloWorld
+            id='angular'
+            frameworkName='Angular.js'
+            title='Superheroic JavaScript MVW Framework'/>
+    </div>,
+    document.getElementById('content')
 )
