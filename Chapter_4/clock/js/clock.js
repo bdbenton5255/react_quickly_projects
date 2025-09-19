5c77c71bff87a3283294d08e2fa45541d862a790
Brandon Benton <brandon.benton.5255@gmail.com>
2025-09-19 15:58:37 -0500

    Tramspile clock and main script to JS

---
+++ Chapter_4/clock/js/clock.js
@@ -0,0 +1,24 @@
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.launchClock();
+        this.state = { currentTime: new Date() };
+    }
+    launchClock() {
+        setInterval(() => {
+            console.log('Updating time...');
+            this.setState({
+                currentTime: new Date()
+            });
+        }, 1000);
+    }
+    render() {
+        console.log('Rendering Clock...');
+        return React.createElement(
+            'div',
+            null,
+            this.state.currentTime
+        );
+    }
+}
+
