5693bb99d8e665f66abe1c38f3bc2bafdbd80642
Brandon Benton <brandon.benton.5255@gmail.com>
2025-09-24 15:56:51 -0500

    constructor properties

---
+++ Chapter_7/elements/jsx/content.jsx
@@ -0,0 +1,12 @@
+class Content extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handleRadio = this.handleRadio.bind(this)
+        this.handleCheckbox = this.handleCheckbox.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleInput = this.handleInput.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleSelectChange = this.handleSelectChange.bind(this)
+        this.handleFirstNameChange = this.handleFirstNameChange.bind(this)
+    }
+}
