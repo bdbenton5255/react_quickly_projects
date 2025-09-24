fab5f7278eda1744c94d8d3b98771704d91762cd
Brandon Benton <brandon.benton.5255@gmail.com>
2025-09-24 16:01:34 -0500

    constructor states

--- Chapter_7/elements/jsx/content.jsx
+++ Chapter_7/elements/jsx/content.jsx
@@ -8,5 +8,21 @@ class Content extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleSelectChange = this.handleSelectChange.bind(this)
         this.handleFirstNameChange = this.handleFirstNameChange.bind(this)
+        this.state = {
+            description: `With the right pattern, applications will be more scalable and easier to maintain. If you aspire one day to become a Node.js architect (or maybe you're already one and want to extend your knowledge), this presentation is for you.`,
+            radioGroup: {
+                angular: false,
+                react: true,
+                polymer: false
+            },
+            checkboxGroup: {
+                node: false,
+                react: true,
+                express: false,
+                mongodb: false
+            },
+            selectedValue: 'node'
+        }
     }
+
 }
