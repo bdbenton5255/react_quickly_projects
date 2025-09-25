26014b7c7f8305b408444746e85f63c4935fdb8f
Brandon Benton <brandon.benton.5255@gmail.com>
2025-09-25 17:04:17 -0500

    content constructor

---
+++ Chapter_7/uncontrolled/jsx/content.jsx
@@ -0,0 +1,7 @@
+class Content extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {textbook: ''}
+        this.handleChange = this.handleChange.bind(this)
+    }
+}
