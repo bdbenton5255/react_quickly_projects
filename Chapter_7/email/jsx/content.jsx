c26324eb33f2ef316e175640a65b261101460a66
Brandon Benton <brandon.benton.5255@gmail.com>
2025-09-27 18:25:53 -0500

    email address form

--- Chapter_7/email/jsx/content.jsx
+++ Chapter_7/email/jsx/content.jsx
@@ -10,4 +10,14 @@ class Content extends React.Component {
         console.log(ReactDOM.findDOMNode(emailAddress).value)
         console.log(ReactDOM.findDOMNode(comments).value)
     }
+    render() {
+        return (
+            <div className="well">
+                <p>{this.prompt}</p>
+                <div className="form-group">
+                    Email: <input ref="emailAddress" className="form-control" type="text" placeholder="hi@azat.co" />
+                </div>
+            </div>
+        )
+    }
 }
