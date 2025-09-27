d8393e63a44f0fc045932e1835020060277f4c7e
Brandon Benton <brandon.benton.5255@gmail.com>
2025-09-27 18:23:48 -0500

    content submit event handler

--- Chapter_7/email/jsx/content.jsx
+++ Chapter_7/email/jsx/content.jsx
@@ -4,4 +4,10 @@ class Content extends React.Component {
         this.submit = this.submit.bind(this)
         this.prompt = 'Please enter your email to win $1,000,000'
     }
+    submit(event) {
+        let emailAddress = this.refs.emailAddress
+        let comments  = this.refs.comments
+        console.log(ReactDOM.findDOMNode(emailAddress).value)
+        console.log(ReactDOM.findDOMNode(comments).value)
+    }
 }
