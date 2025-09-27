a8d108bcbdc227e42c83b09e15efa989a1bca984
Brandon Benton <brandon.benton.5255@gmail.com>
2025-09-27 18:22:33 -0500

    content constructor

---
+++ Chapter_7/email/jsx/content.jsx
@@ -0,0 +1,7 @@
+class Content extends React.Component {
+    constructor(props) {
+        super(props)
+        this.submit = this.submit.bind(this)
+        this.prompt = 'Please enter your email to win $1,000,000'
+    }
+}
