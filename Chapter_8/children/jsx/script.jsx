6c13ea81eeae44506a4f67bfe8c0180419dcf554
Brandon Benton <brandon.benton.5255@gmail.com>
2025-09-28 15:44:48 -0500

    Content image object

--- Chapter_8/children/jsx/script.jsx
+++ Chapter_8/children/jsx/script.jsx
@@ -4,5 +4,13 @@ ReactDOM.render(
             <h1>React</h1>
             <p>Rocks</p>
         </Content>
+        <Content>
+            <img src="images/azat.jpg" width="100"/>
+        </Content>
+        <Content>
+        </Content>
+        <Content>
+
+        </Content>
     </div>
 )
