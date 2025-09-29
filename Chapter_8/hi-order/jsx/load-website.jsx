638aff4f87c6db41741bb7550ecc0c63fac8ebe6
Brandon Benton <brandon.benton.5255@gmail.com>
2025-09-28 23:35:46 -0500

    LoadWebsite class constructor and properties

---
+++ Chapter_8/hi-order/jsx/load-website.jsx
@@ -0,0 +1,8 @@
+const LoadWebsite = (Component) => {
+    class _LoadWebsite extends React.Component {
+        constructor(props) {
+            super(props)
+            this.state = {label: 'Run', handleClick: this.handleClick.bind(this)}
+        }
+    }
+}
